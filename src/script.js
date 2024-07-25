ef73394c0a4b7f10041e39a604fc8a685395b418
Jojo-star7 <jojonabu49@gmail.com>
2024-07-25 22:55:33 +0300

    added the basic form handling and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-alert("hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "A Light exists in Spring",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form-generator");
+poemFormElement.addEventListener("submit", generatePoem);
